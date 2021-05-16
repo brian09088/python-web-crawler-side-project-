0050a7fa8f4d67b7bab705ea3ff4d6ea2432440a
Brian Su <brian18088@gmail.com>
2021-05-16 13:23:00 +0800

    Add files via upload

---
+++ py資料夾/希崎潔西卡(希崎ジェシカ).py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import requests
+import time
+from bs4 import BeautifulSoup
+import random
+start=1 #起始頁面
+times=10 #抓取頁數
+end=start+times #終止頁數
+
+for i in range(start,end,1):
+    link="https://www.jav321.com/star/26774/"+str(i)
+    jav = requests.get(link)
+    time.sleep(random.randint(1,10))
+    soup = BeautifulSoup(jav.text,"html.parser")
+    name = soup.find_all("a")
+    for a in name:
+        if(len(a.text.split())>1 and a.text!="← 前の" and a.text!="→ 次の"):
+            print(a.text)
+
